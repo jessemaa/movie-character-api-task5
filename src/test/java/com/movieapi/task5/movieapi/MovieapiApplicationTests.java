b67793a20181e727d598ae80f9d91390bf2a34e9
Jesse Saarimaa <mewwbae@gmail.com>
2021-02-10 14:23:29 +0200

    created the project

---
+++ src/test/java/com/movieapi/task5/movieapi/MovieapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieapi.task5.movieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
